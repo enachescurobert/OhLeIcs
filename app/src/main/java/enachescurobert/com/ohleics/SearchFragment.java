84bcf633e65c38022d50afe037b52a452bef879e
Enachescu Robert <enachescurobert@gmail.com>
2019-06-04 23:39:02 +0300

    Error while getting the data

--- app/src/main/java/enachescurobert/com/ohleics/SearchFragment.java
+++ app/src/main/java/enachescurobert/com/ohleics/SearchFragment.java
@@ -1,37 +1,77 @@
 package enachescurobert.com.ohleics;
 
 import android.content.Intent;
-import android.media.Image;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import enachescurobert.com.ohleics.models.HitsList;
+import enachescurobert.com.ohleics.models.HitsObject;
+import enachescurobert.com.ohleics.models.Post;
+import enachescurobert.com.ohleics.util.ElasticSearchApi;
+import okhttp3.Credentials;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by User on 10/22/2017.
+ */
 
 public class SearchFragment extends Fragment {
 
     private static final String TAG = "SearchFragment";
+    private static final String BASE_URL = "https://35.228.155.106//elasticsearch/posts/";
 
     //widgets
     private ImageView mFilters;
+    private EditText mSearchText;
 
+    //vars
+    private String mElasticSearchPassword;
+    private String mPrefCity;
+    private String mPrefStateProv;
+    private String mPrefCountry;
+    private ArrayList<Post> mPosts;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-
         mFilters = (ImageView) view.findViewById(R.id.ic_search);
+        mSearchText = (EditText) view.findViewById(R.id.input_search);
+
+        getElasticSearchPassword();
         init();
 
         return view;
     }
-    
-    public void init(){
+
+    private void init(){
         mFilters.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +80,129 @@ public class SearchFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        ||actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+
+                    mPosts = new ArrayList<Post>();
+
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl(BASE_URL)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+
+                    ElasticSearchApi searchAPI = retrofit.create(ElasticSearchApi.class);
+
+                    HashMap<String, String> headerMap = new HashMap<String, String>();
+                    headerMap.put("Authorization", Credentials.basic("user", mElasticSearchPassword));
+
+                    String searchString = "";
+
+                    if(!mSearchText.equals("")){
+                        searchString = searchString + mSearchText.getText().toString() + "*";
+                    }
+                    if(!mPrefCity.equals("")){
+                        searchString = searchString + " city:" + mPrefCity;
+                    }
+                    if(!mPrefStateProv.equals("")){
+                        searchString = searchString + " state_province:" + mPrefStateProv;
+                    }
+                    if(!mPrefCountry.equals("")){
+                        searchString = searchString + " country:" + mPrefCountry;
+                    }
+
+                    Call<HitsObject> call = searchAPI.search(headerMap, "AND", searchString);
+
+                    call.enqueue(new Callback<HitsObject>() {
+                        @Override
+                        public void onResponse(Call<HitsObject> call, Response<HitsObject> response) {
+
+                            HitsList hitsList = new HitsList();
+                            String jsonResponse = "";
+                            try{
+                                Log.d(TAG, "onResponse: server response: " + response.toString());
+
+                                if(response.isSuccessful()){
+                                    hitsList = response.body().getHits();
+                                }else{
+                                    jsonResponse = response.errorBody().string();
+                                }
+
+                                Log.d(TAG, "onResponse: hits: " + hitsList);
+
+                                for(int i = 0; i < hitsList.getPostIndex().size(); i++){
+                                    Log.d(TAG, "onResponse: data: " + hitsList.getPostIndex().get(i).getPost().toString());
+                                    mPosts.add(hitsList.getPostIndex().get(i).getPost());
+                                }
+
+                                Log.d(TAG, "onResponse: size: " + mPosts.size());
+                                //setup the list of posts
+
+                            }catch (NullPointerException e){
+                                Log.e(TAG, "onResponse: NullPointerException: " + e.getMessage() );
+                            }
+                            catch (IndexOutOfBoundsException e){
+                                Log.e(TAG, "onResponse: IndexOutOfBoundsException: " + e.getMessage() );
+                            }
+                            catch (IOException e){
+                                Log.e(TAG, "onResponse: IOException: " + e.getMessage() );
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<HitsObject> call, Throwable t) {
+                            Log.e(TAG, "onFailure: " + t.getMessage() );
+                            Toast.makeText(getActivity(), "search failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                return false;
+            }
+        });
     }
-    
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getFilters();
+    }
+
+    private void getElasticSearchPassword(){
+        Log.d(TAG, "getElasticSearchPassword: retrieving elasticsearch password.");
+
+        Query query = FirebaseDatabase.getInstance().getReference()
+                .child(getString(R.string.node_elasticsearch))
+                .orderByValue();
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot singleSnapshot = dataSnapshot.getChildren().iterator().next();
+                mElasticSearchPassword = singleSnapshot.getValue().toString();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getFilters(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mPrefCity = preferences.getString(getString(R.string.preferences_city), "");
+        mPrefStateProv = preferences.getString(getString(R.string.preferences_state_province), "");
+        mPrefCountry = preferences.getString(getString(R.string.preferences_country), "");
+
+        Log.d(TAG, "getFilters: got filters: \ncity: " + mPrefCity + "\nState/Prov: " + mPrefStateProv
+                + "\nCountry: " + mPrefCountry);
+    }
+
 }
