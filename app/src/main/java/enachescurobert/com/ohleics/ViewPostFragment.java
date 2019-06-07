3a9d31826c6821d2d9b75a75cdfc0292637f6ad6
Enachescu Robert <Robert.Enachescu@hogarthww.com>
2019-06-07 16:33:54 +0300

    Inflating the fragment with the Post Details

--- app/src/main/java/enachescurobert/com/ohleics/ViewPostFragment.java
+++ app/src/main/java/enachescurobert/com/ohleics/ViewPostFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,16 @@ import android.view.ViewGroup;
 public class ViewPostFragment extends Fragment {
 
     private static final String TAG = "ViewPostFragment";
+
+    //vars
+    private String mPostId;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPostId = (String) getArguments().get(getString(R.string.arg_post_id));
+        Log.d(TAG, "onCreate: got the post id: " + mPostId);
+    }
 
     @Nullable
     @Override
--- app/src/main/java/enachescurobert/com/ohleics/util/PostListAdapter.java
+++ app/src/main/java/enachescurobert/com/ohleics/util/PostListAdapter.java
@@ -1,6 +1,7 @@
 package enachescurobert.com.ohleics.util;
 
 import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +11,8 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 
 import enachescurobert.com.ohleics.R;
+import enachescurobert.com.ohleics.SearchActivity;
+import enachescurobert.com.ohleics.SearchFragment;
 import enachescurobert.com.ohleics.models.Post;
 
 public class PostListAdapter extends RecyclerView.Adapter<PostListAdapter.ViewHolder>{
@@ -47,14 +50,40 @@ public class PostListAdapter extends RecyclerView.Adapter<PostListAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
         UniversalImageLoader.setImage(mPosts.get(position).getImage(), holder.mPostImage);
 
+        final int pos = position;
         holder.mPostImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: selected a post");
                 //TODO
+
+                //view the post in more detail
+                Fragment fragment = (Fragment)((SearchActivity)mContext).getSupportFragmentManager()
+                        .findFragmentByTag("android:switcher:" + R.id.viewpager_container+ ":" +
+                                ((SearchActivity)mContext).mViewPager.getCurrentItem());
+
+                //fragment 0 is in the search fragment
+                //fragment 1 is in the watch list fragment
+
+                if(fragment != null){
+                    //SearchFragment (#0)
+                    if(fragment.getTag().equals("android:switcher:" + R.id.viewpager_container + ":0")){
+                        Log.d(TAG, "onClick: switching to: " + mContext.getString(R.string.fragment_view_post));
+
+                        SearchFragment searchFragment = (SearchFragment)((SearchActivity)mContext).getSupportFragmentManager()
+                                .findFragmentByTag("android:switcher:" + R.id.viewpager_container+ ":" +
+                                        ((SearchActivity)mContext).mViewPager.getCurrentItem());
+                        searchFragment.viewPost(mPosts.get(pos).getPost_id());
+                    }
+                    //Watchlist Fragment( #1)
+                    else if(fragment.getTag().equals("android:switcher:" + R.id.viewpager_container + ":1")){
+                        Log.d(TAG, "onClick: switching to: " + mContext.getString(R.string.fragment_watch_list));
+
+                    }
+                }
             }
         });
     }
